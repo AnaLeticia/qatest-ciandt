7e9adf449d2db7a63101abfd79f378ac8b13706f
Ana Leticia Herculano <ana.herculano@fastalelo.onmicrosoft.com>
2021-04-16 22:44:45 -0300

    Esqueleto da feature ok

--- src/test/java/br/com/ciandt/StepDefinitions.java
+++ src/test/java/br/com/ciandt/StepDefinitions.java
@@ -3,9 +3,68 @@ package br.com.ciandt;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
 
+	@Dado("que navego para a página de busca do banco de questões")
+	public void navegar() {
+		System.out.println("Abrindo o browser na pagina de busca");
+	}
+	
+	@E("digito 'Science: Computers' no campo de busca")
+	public void digitar() {
+		System.out.println("Digitanto no campo de busca");
+	}
+
+	@Quando("clico no botão de buscar")
+	public void clicar() {
+		System.out.println("Clicando no botão Buscar");
+	}
+
+	@Entao("visualizo uma mensagem de erro com o texto 'No questions found.")
+	public void visualizar() {
+		System.out.println("Visualizando a mensagem");
+	}
+
+	// Cenário Categoria 
+	
+	@Dado("que navego para a página de busca do banco de categoria")
+	public void navegarc() {
+		System.out.println("Abrindo o browser na pagina de busca de categoria");
+	}
+	
+//	@E("digito 'Science: Computers' no campo de busca")
+//	public void digitarc() {
+//		System.out.println("Digitanto no campo de busca de categoria");
+//	}
+
+//	@Quando("clico no botão de buscar")
+//	public void clicarc() {
+//		System.out.println("Clicando no botão Buscar de categoria");
+//	}
+
+	@Entao("visualizo uma listagem de questões com {string} itens")
+	public void quantidade(String numero) {
+		System.out.println("Visualizando listagem com as quantidades de questões");
+	}
+
+	@E("verifico o controle de paginação")
+	public void paginacao() {
+		System.out.println("controle de paginação está ativo");
+	}
+	
+	
+		
 }
+	
+	
+	
+	
+
+
---
+++ src/test/resources/br/com/ciandt/features/BuscaBancoQuestões.feature
@@ -0,0 +1,27 @@
+#language: pt
+
+Funcionalidade: Busca no Banco de Questões
+
+Esquema do Cenario: <CT> - <Descrição do cenario> 
+	Dado que navego para a página de busca do banco de questões
+	E digito 'Science: Computers' no campo de busca
+	Quando clico no botão de buscar 
+	Entao visualizo uma mensagem de erro com o texto 'No questions found.
+
+Exemplos:
+	| CT 	| Descrição do cenario					| 
+	| 01  | Busca por Questão Inexistente |
+
+#Precisamos fazer uma busca na categoria por Science: Computers e verificar se a listagem de questões está com 25 itens e se o controle de paginação irá aparecer.
+
+Esquema do Cenario: <CT> - <Descrição do cenario> 
+	Dado que navego para a página de busca do banco de categoria
+	E digito 'Science: Computers' no campo de busca
+	Quando clico no botão de buscar 
+	Entao visualizo uma listagem de questões com "<numero>" itens
+	E verifico o controle de paginação
+
+Exemplos: 
+	| CT 	| Descrição do cenario 					|  numero  |
+	| 02  | Busca por Categoria						|  25 		 |
+ 
